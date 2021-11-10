998d6268a2307f40c6d707c522d58e9ffed94efd
Tiago Silva <tiagodusilva@gmail.com>
2021-11-10 12:25:27 +0000

    Improved netflix titles cleanup Altered analysis

--- cleanup.py
+++ cleanup.py
@@ -2,6 +2,16 @@ import pandas as pd
 
 titles = pd.read_csv('data/netflix_titles.csv')
 
-titles[titles['duration'].isnull()].rename(columns={'rating':'duration','duration':'rating'})
+# Fix columns in wrong order
+idx = titles['duration'].isnull()
+titles.loc[idx, 'duration'] = titles['rating']
+titles.loc[idx, 'rating'] = 'NR'
+
+# Fix missing values in rating
+titles.loc[titles['rating'].isnull(), 'rating'] = 'NR'
+titles.loc[titles['rating'] == 'UR', 'rating'] = 'NR'
+
+# Fix incorrect list formatting
+titles.loc[:, 'country'] = titles.loc[:, 'country'].str.lstrip(to_strip=', ')
 
 titles.to_csv('data/netflix_titles_clean.csv')
